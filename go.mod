ddb2dfdc12bc619b6e1ee6b339431fc52656c6a3
RCBONFIM <RCBONFIM@tce.mt.gov.br>
2025-08-14 09:23:27 -0400

    Exercise completed

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/RodrigoCelso/gophercises-04
 
 go 1.24.4
+
+require golang.org/x/net v0.43.0
--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
 
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func main() {
+	file, err := os.Open("ex4.html")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	doc, err := html.Parse(file)
+	if err != nil {
+		panic(err)
+	}
+	var linkParsed []Link
+	for d := range doc.Descendants() {
+		if d.DataAtom == atom.A {
+			link := Link{}
+			for _, attr := range d.Attr {
+				if attr.Key == "href" {
+					link.Href = attr.Val
+				}
+			}
+			for a := range d.Descendants() {
+				if a.Type == html.TextNode {
+					link.Text = fmt.Sprint(link.Text, strings.TrimSpace(a.Data))
+				}
+			}
+			linkParsed = append(linkParsed, link)
+		}
+	}
+
+	fmt.Println(linkParsed)
 }
